7b41390ca30751d3b0daaf98671426a1d79fff77
pjo13 <43048127+pjo13@users.noreply.github.com>
2019-09-24 14:36:31 -0400

    Update main.cpp
    added Milo to vector

--- main.cpp
+++ main.cpp
@@ -115,7 +115,7 @@ int main() {
 	gTex.push_back(loadImage("Assets/cs1666_pep24.png"));
 	gTex.push_back(loadImage("Assets/Keith C Stebler.png"));
 	gTex.push_back(loadImage("Assets/physics.png"));
-	
+	gTex.push_back(loadImage("Assets/CS1666_MCD66.png"));
 	gTex.push_back(loadImage("Assets/cs1666_kel177.png"));
 	gTex.push_back(loadImage("Assets/CS1666_Linghai.png"));
 	gTex.push_back(loadImage("Assets/CS1666_apw50.png"));
