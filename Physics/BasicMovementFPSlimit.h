74f0528da007748d83ab35b49c9f73c06ede193b
MichaelSpila97 <mrs185@pitt.edu>
2019-10-01 23:09:24 -0400

    Moved all functionaly involving movement to BasicMovementFPSLimit.cpp from main

---
+++ Physics/BasicMovementFPSlimit.h
@@ -0,0 +1,9 @@
+
+
+
+#include "../General/gpEntity.h"
+
+bool handleKeyEvents(SDL_Event e, gpEntity &ent);
+void handleKeyUpEvent(SDL_Event e, gpEntity &ent);
+void handleKeyDownEvent(SDL_Event e, gpEntity &ent);
+
