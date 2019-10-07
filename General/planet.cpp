75c9b10f69440e2a4f7f37b4a9097377462b93e2
pjo13 <43048127+pjo13@users.noreply.github.com>
2019-10-07 13:59:48 -0400

    Adding Planet Implementation

---
+++ General/planet.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <string>
+#include <tuple>
+using namespace std;
+
+class Planet
+{
+	private:
+		int radius;
+		int velocity;
+		float direction;
+		int gravity;
+		tuple<int, int> position;
+		sting sprite;
+
+		public:
+			int getRadius()
+			{
+				return radius;
+			}
+
+			int getVelocity()
+			{
+				return velocity;
+			}
+
+			float getDirection()
+			{
+				return direction;
+			}
+
+			int getGravity()
+			{
+				return gravity;
+			}
+
+			tuple getPosition()
+			{
+				return position;
+			}
+
+			string getSprite()
+			{
+				return sprite;
+			}
+
+			void setRadius(int r)
+			{
+				radius = r;
+			}
+
+			void setVelocity(int v)
+			{
+				velocity = v;
+			}
+
+			void setDirection(float d)
+			{
+				direction = d;
+			}
+
+			void setGravity(int g)
+			{
+				gravity = g;
+			}
+
+			void setPosition(tuple<int, int> newPos)
+			{
+				position = newPos;
+			}
+
+			void setSprite(string s)
+			{
+				sprite = s;
+			}
+
+}
