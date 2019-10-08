b335ca5095e5447b4f8999035d15ade059f2f980
apw50 <apw50@pitt.edu>
2019-10-07 20:49:53 -0400

    basic box collisions now work

--- Physics/BasicMovementFPSlimit.h
+++ Physics/BasicMovementFPSlimit.h
@@ -5,4 +5,6 @@
 bool handleKeyEvents(SDL_Event e, gpEntity &ent);
 void handleKeyUpEvent(SDL_Event e, gpEntity &ent);
 void handleKeyDownEvent(SDL_Event e, gpEntity &ent);
-void updatePosition(gpEntity &ent);
+bool check_collision(SDL_Rect* a, SDL_Rect* b);
+bool check_all_collisions(SDL_Rect* a, std::vector<gpEntity*> &osEntity);
+void updatePosition(gpEntity &ent, std::vector<gpEntity*> &osEntity);
--- aipg.cpp
+++ aipg.cpp
@@ -60,7 +60,7 @@ int main(){
 		while(SDL_PollEvent(&e)) {
 			gameon = handleKeyEvents(e, playerent);	
 		}
-		updatePosition(playerent);
+		updatePosition(playerent, osEntity);
 
 		//---------------COLLISION SHOULD BE HANDLED HERE------------------------
 		//Adjusts the players entities pos based on interal values
