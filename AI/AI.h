56b47595e052ae34d9057abf2dd52c314e6e56f3
jcz18 <jcz18@pitt.edu>
2019-10-13 16:33:14 -0400

    more underlying work for ai loop, queue stuff

--- AI/AI.h
+++ AI/AI.h
@@ -3,7 +3,7 @@
 #include <iostream>
 #include <string>
 #include <vector>
-
+#include "./Queue.h"
 #include "../General/Ship.h"
 //#include "Physics/BasicMovementFPSlimit.h"
 using namespace std;
@@ -12,10 +12,11 @@ class AI
 {
     private:
         vector<Ship> __ships;
-//	vector<int,int> storedMapState; //probably needs to be changed
+	vector<vector<int>> storedMapState; //probably needs to be changed
 
     public:
         void setShips(vector<Ship> newShips);
-//	bool checkMapState(vector <int,int> newState); //change mapstate
+	bool checkMapState(vector<vector<int>> newState); //change mapstate
         void setShipPath(Ship shipToPath);
+	Queue calculatePath(Ship theShip, vector<int> destination);
 };
