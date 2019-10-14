40d154254eeaa00e68c6302f210cedf759f6e016
gjcjchapmn <garethjameschapman@gmail.com>
2019-10-13 21:17:26 -0400

    starting path finding

--- AI/AI.cpp
+++ AI/AI.cpp
@@ -7,5 +7,37 @@
 
         void AI::setShipPath(Ship shipToPath)
         {
+        	//get start and end points
+        	vector<int> start = shipToPath.getPosition();
+        	vector<int> destination = shipToPath.getDestination();
+
+        	//using built in queue c++ data structure for now
+        	std::queue<vector<int> > open;
+
+        	open.push(start);
+
+        	while (open.size() != 0)
+        	{
+        		vector<int> s = open.pop();
+
+        		if (s == destination)
+        		{
+        			return;
+        		}
+
+        		
+        	}
+
+        }
+
+    	void AI::lineOfSight()
+    	{
+
+    	}
+
+        bool checkMapState(vector<vector<int> > newState)
+        {
+
+        	return false;
 
         }
