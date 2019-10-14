87505de5744f755884bff2f33ae9f82a207377b6
kel243 <54601458+kel243@users.noreply.github.com>
2019-10-14 12:29:44 -0400

    changed variable names

--- Physics/BasicGravity.h
+++ Physics/BasicGravity.h
@@ -1,5 +1,5 @@
 #include "../General/Sprite.h"
 #pragma once
 
-void gravity_pull(Sprite &playerent, Sprite &bodyent);
+void gravity_pull(Sprite &body1, Sprite &body2);
 void updatePositionGrav(Sprite &ent, std::vector<Sprite*> &osSprite, int ZONE_WIDTH, int ZONE_HEIGHT);
