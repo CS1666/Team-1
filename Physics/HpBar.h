54d7033ea24cc8573d23aa51ed4fb5da03104c82
kel243 <54601458+kel243@users.noreply.github.com>
2019-10-23 15:01:07 -0400

    Update HpBar.h

--- Physics/HpBar.h
+++ Physics/HpBar.h
@@ -1,2 +1,2 @@
 void RenderHPBar(int x, int y, int w, int h, float Percent, SDL_Color FGColor, SDL_Color BGColor);
-SDL_Color color(Uint8 r, Uint8 g, Uint8 b, Uint8 a);
+SDL_Color color(Uint8 r, Uint8 g, Uint8 b);
