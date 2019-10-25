46f7f768d3966294cea9b53eaaeae815fe8312d4
kel243 <54601458+kel243@users.noreply.github.com>
2019-10-24 20:06:53 -0400

    hp updated

--- General/HpBar.cpp
+++ General/HpBar.cpp
@@ -2,7 +2,7 @@
 #include <SDL.h>
 
 	HpBar::HpBar() {};
-	HpBar::HpBar(SDL_Rect dBox, SDL_Texture* aTex, float new_percentage):drawbox{dBox}, assetTex{aTex},  percentage{new_percentage}, x{(float)dBox.x}, y{(float)dBox.y}{};
+	HpBar::HpBar(SDL_Rect dBox, SDL_Texture* aTex, float percentage): Sprite(dBox, aTex), percentage{percentage}{};
 	HpBar::~HpBar()
 	{
 		SDL_DestroyTexture(assetTex);
@@ -16,71 +16,6 @@
 	float HpBar::getPercentage(){
 		return percentage;
 	}
-
-	void HpBar::setX(int x)
-	{
-		HpBar::x = (float)x;
-		drawbox.x = (int)HpBar::x;
-	}
-
-	void HpBar::setY(int y)
-	{
-		HpBar::y = (float)y;
-		drawbox.y = (int)HpBar::y;
-	}
-
-
-	void HpBar::setX(float x){
-		HpBar::x = x;
-		drawbox.x = (int)HpBar::x;
-	}
-
-	int HpBar::getX(){
-			return drawbox.x;
-	}
-
-	void HpBar::setY(float y){
-		HpBar::y = y;
-		drawbox.y = (int)HpBar::y;
-	}
-	int HpBar::getY(){
-		return drawbox.y;
-	}
 	
-	float HpBar::getTrueX()
-	{
-		return HpBar::x;
-	}
-
-	float HpBar::getTrueY()
-	{
-		return HpBar::y;
-	}
-
-	int HpBar::getW(){
-		return drawbox.w;
-	}
-
-	int HpBar::getH(){
-		return drawbox.h;
-	}
-	void HpBar::setW(int new_w){
-		drawbox.w = new_w;
-	}
-	void HpBar::setH(int new_h){
-		drawbox.h = new_h;
-	}
-
-	void HpBar::setTexture(SDL_Texture* tex){
-		assetTex = tex;
-	}
-
-	SDL_Texture* HpBar::getTexture(){
-			return assetTex;
-	}
-
-	SDL_Rect* HpBar::getDrawBox(){
-		return &drawbox;
-	}
 
 
--- General/HpBar.h
+++ General/HpBar.h
@@ -3,40 +3,23 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include "Sprite.h"
 
 using namespace std;
 
-class HpBar 
+class HpBar : public Sprite 
 {
     private:
         
-	SDL_Rect drawbox;
-	SDL_Texture* assetTex;
 	float percentage;
-	float x, y;
 
     public:
         HpBar();
         HpBar(SDL_Rect dBox, SDL_Texture* aTex, float percentage);
         ~HpBar();
         
-        void setPercentage(float newPercentage);
+        void setPercentage(float Percentage);
         float getPercentage();
 
-	void setX(int new_x);
-	void setY(int new_y);
-	int getX();
-	int getY();
-	void setX(float new_x);
-	void setY(float new_y);
-	float getTrueX();
-	float getTrueY();
-	int getW();
-	int getH();
-	void setW(int new_w);
-	void setH(int new_h);
-	void setTexture(SDL_Texture* tex);
-	SDL_Texture* getTexture();
-	SDL_Rect* getDrawBox();
 };
 
