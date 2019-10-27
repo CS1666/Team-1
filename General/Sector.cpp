9bdffdabb1791755b719053cecdab005f3220728
gjcjchapmn <garethjameschapman@gmail.com>
2019-10-27 14:52:39 -0400

    Changes how map state is created

--- General/Sector.cpp
+++ General/Sector.cpp
@@ -20,3 +20,19 @@ vector<int> Sector::getSize()
 {
 	return __size;
 }
+
+vector<vector<int> > Sector::getState()
+{
+	std::vector<std::vector<int> > currentState;
+
+	for (Star star : __stars)
+	{
+		vector<int> size = star.getSize();
+		vector<int> position = star.getPosition();
+
+		currentState.push_back({position[0], position[1], size[0], size[1]});
+
+	}
+
+	return currentState;
+}
