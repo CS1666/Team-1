157a9fa3517614b2b6c1ec4fdaeab442503166de
pjo13 <43048127+pjo13@users.noreply.github.com>
2019-11-27 22:11:05 -0500

    Update demo.cpp

--- General/demo.cpp
+++ General/demo.cpp
@@ -285,62 +285,53 @@ void run_demo(gpRender gr){
 	//Sector 1
 	SDL_Texture* sector1Tex = gr.loadImage("Assets/Objects/enemySector.png");
 	SDL_Rect sector1Rect = {1184,25,15,15};
-	Sprite sector1ent(sector1Rect, sector1Tex);
-	sector1ent.isUI();
+	HpBar sector1ent(sector1Rect, sector1Tex, 0);
 	osSprite.push_back(&sector1ent);
 	//Sector 2
 	SDL_Texture* sector2Tex = gr.loadImage("Assets/Objects/enemySector.png");
 	SDL_Rect sector2Rect = {1213,25,15,15};
-	Sprite sector2ent(sector2Rect, sector2Tex);
-	sector2ent.isUI();
+	HpBar sector2ent(sector2Rect, sector2Tex, 0);
 	osSprite.push_back(&sector2ent);
 	//Sector 3
 	SDL_Texture* sector3Tex = gr.loadImage("Assets/Objects/enemySector.png");
 	SDL_Rect sector3Rect = {1242,25,15,15};
-	Sprite sector3ent(sector3Rect, sector3Tex);
-	sector3ent.isUI();
+	HpBar sector3ent(sector3Rect, sector3Tex, 0);
 	osSprite.push_back(&sector3ent);
 	//Sector 4
 	SDL_Texture* sector4Tex = gr.loadImage("Assets/Objects/enemySector.png");
 	SDL_Rect sector4Rect = {1184,52,15,15};
-	Sprite sector4ent(sector4Rect, sector4Tex);
-	sector4ent.isUI();
+	HpBar sector4ent(sector4Rect, sector4Tex, 0);
 	osSprite.push_back(&sector4ent);
 	//Sector 5
 	SDL_Texture* sector5Tex = gr.loadImage("Assets/Objects/enemySector.png");
 	SDL_Rect sector5Rect = {1213,52,15,15};
-	Sprite sector5ent(sector5Rect, sector5Tex);
-	sector5ent.isUI();
+	HpBar sector5ent(sector5Rect, sector5Tex, 0);
 	osSprite.push_back(&sector5ent);
 	//Sector 6
 	SDL_Texture* sector6Tex = gr.loadImage("Assets/Objects/enemySector.png");
 	SDL_Rect sector6Rect = {1242,52,15,15};
-	Sprite sector6ent(sector6Rect, sector6Tex);
-	sector6ent.isUI();
+	HpBar sector6ent(sector6Rect, sector6Tex, 0);
 	osSprite.push_back(&sector6ent);
 	//Sector 7
 	SDL_Texture* sector7Tex = gr.loadImage("Assets/Objects/enemySector.png");
 	SDL_Rect sector7Rect = {1184,79,15,15};
-	Sprite sector7ent(sector7Rect, sector7Tex);
-	sector7ent.isUI();
+	HpBar sector7ent(sector7Rect, sector7Tex, 0);
 	osSprite.push_back(&sector7ent);
 	//Sector 8
 	SDL_Texture* sector8Tex = gr.loadImage("Assets/Objects/currentSector.png");
 	SDL_Rect sector8Rect = {1213,79,15,15};
-	Sprite sector8ent(sector8Rect, sector8Tex);
-	sector8ent.isUI();
+	HpBar sector8ent(sector8Rect, sector8Tex, 0);
 	osSprite.push_back(&sector8ent);
 	//Sector 9
 	SDL_Texture* sector9Tex = gr.loadImage("Assets/Objects/enemySector.png");
 	SDL_Rect sector9Rect = {1242,79,15,15};
-	Sprite sector9ent(sector9Rect, sector9Tex);
-	sector9ent.isUI();
+	HpBar sector9ent(sector9Rect, sector9Tex, 0);
 	osSprite.push_back(&sector9ent);
 	//current sector
 	int curSector = 8;
 	
 	SDL_Texture* mapTexs[] = {sector1Tex, sector2Tex, sector3Tex, sector4Tex, sector5Tex, sector6Tex, sector7Tex, sector8Tex, sector9Tex};
-	Sprite mapSprites[] = {sector1ent, sector2ent, sector3ent, sector4ent, sector5ent, sector6ent, sector7ent, sector8ent, sector9ent};
+	HpBar mapSprites[] = {sector1ent, sector2ent, sector3ent, sector4ent, sector5ent, sector6ent, sector7ent, sector8ent, sector9ent};
 	
 	/*
 	//Ship Cruiser initilization
